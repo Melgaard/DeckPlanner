18043aa9e02eba09164061d870ab07b9b8bf06ab
unknown <marcusmelgaardj@gmail.com>
2021-02-08 14:16:28 +0100

    First day

---
+++ src/components/Deck.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="deck" >
+		This is {{name}}
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Deck',
+  props: {
+	name: String,
+	image: String, //TODO
+  },
+  methods: {
+	// clicked() {
+	// 	console.log('clicked ' + name);
+	// 	this.$emit('clicked', name)
+	// }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.deck {
+	display: inline-block;
+	width: 375px;
+	height: 275px;
+}
+</style>
