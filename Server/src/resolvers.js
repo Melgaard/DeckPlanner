51d652d48d0c35fb490dc95a7f5df25688b2ca29
Melgaard <marcusmelgaardj@gmail.com>
2024-07-04 13:20:19 +0200

    alphabetically sort maindeck

--- Server/src/resolvers.js
+++ Server/src/resolvers.js
@@ -2,10 +2,27 @@
 module.exports = {
 	Query: {
 		decklistByID: async (_, { id }, { dataSources }) => {
+			console.log('0')
 			return await dataSources.decklistAPI.getByID({ id });
 		},
 		decklistsByUser: async (_, { id }, { dataSources }) => {
-			return await dataSources.decklistAPI.getAllDecklistsByUser({ id });
+			console.log('1')
+
+			let decklists = await dataSources.decklistAPI.getAllDecklistsByUser({ id });
+
+			//TODO: Sorted alphabetically. Make card type sort and mana value sort
+			//TODO: Sort Sideboard too
+			//TODO: Figure out where the user should choose sort type.
+			for (const deck of decklists) {
+				deck.mainDeck = deck.mainDeck.sort((card, card2) => {
+					if (card.name > card2.name) return 1
+					return -1
+				})
+			}
+
+			
+
+			return decklists
 		},
 		me: (_, __, { dataSources }) => 
 			dataSources.userAPI.getUser()
